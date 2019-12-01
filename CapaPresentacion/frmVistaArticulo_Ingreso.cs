29cc3f4637523d68b634d18d7822c79f0fbc6fa6
alex00324 <aangulo2418@gmail.com>
2019-12-01 15:17:58 -0500

    programa base estable

---
+++ CapaPresentacion/frmVistaArticulo_Ingreso.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class frmVistaArticulo_Ingreso : Form
+    {
+        private bool IsNuevo = false;
+        private bool IsEditar = false;
+        public frmVistaArticulo_Ingreso()
+        {
+            InitializeComponent();
+            this.ttMensaje.SetToolTip(this.txtNombre, "Ingrese el Nombre del Artículo");
+            this.ttMensaje.SetToolTip(this.pxImagen, "Selecciona la Imagen del Artículo");
+            this.ttMensaje.SetToolTip(this.cbIdcategoria, "Seleccione la Categoria del Artículo");
+            this.ttMensaje.SetToolTip(this.cbIdpresentacion, "Seleccione la Presentacion del Artículo");
+            this.txtIdcategoria.Visible = false;
+            this.txtCodigo.Visible = false;
+            this.label6.Visible = false;
+            this.txtIdcategoria.ReadOnly = true;
+            this.LlenarComboPresentacion();
+            this.LlenarComboCategoria();
+        }
+       //Mostrar Mensaje de Confirmacion 
+        private void MensajeOK(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        //Mostrar Mensaje de Error
+
+        private void MensajeError(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        //Limpiar todos los controles del formulario
+        private void Limpiar()
+        {
+            this.txtCodigo.Text = string.Empty;
+            this.txtNombre.Text = string.Empty;
+            this.txtDescripcion.Text = string.Empty;
+            this.txtIdcategoria.Text = string.Empty;
+          //  this.txtCategoria.Text = string.Empty;
+            this.txtIdarticulo.Text = string.Empty;
+            this.pxImagen.Image = global::CapaPresentacion.Properties.Resources.file;
+
+        }
+        //Habilitar los controles del formulario
+        private void Habilitar(bool valor)
+        {
+
+            this.txtCodigo.ReadOnly = !valor;
+            this.txtNombre.ReadOnly = !valor;
+            this.txtDescripcion.ReadOnly = !valor;
+            this.cbIdcategoria.Enabled = valor;
+            this.cbIdpresentacion.Enabled = valor;
+            this.btnCargar.Enabled = valor;
+            this.btnLimpiar.Enabled = valor;
+            this.txtIdarticulo.ReadOnly = !valor;
+        }
+        //Habilitar los Botones
+        private void Botones()
+        {
+            if (this.IsNuevo || this.IsEditar)
+            {
+                this.Habilitar(true);
+                this.btnNuevo.Enabled = false;
+                this.btnGuardar.Enabled = true;
+                this.btnEditar.Enabled = false;
+                this.btnCancelar.Enabled = true;
+            }
+            else
+            {
+                this.Habilitar(false);
+                this.btnNuevo.Enabled = true;
+                this.btnGuardar.Enabled = false;
+                this.btnEditar.Enabled = true;
+                this.btnCancelar.Enabled = false;
+            }
+        }
+        private void frmVistaArticulo_Ingreso_Load(object sender, EventArgs e)
+        {
+            
+            this.Mostrar();
+            this.Habilitar(false);
+            this.Botones();
+        }
+        //Metodo para ocultar columnas
+        private void OcultarColumnas()
+        {
+            this.dataListado.Columns[0].Visible = false;
+            this.dataListado.Columns[1].Visible = false;
+            this.dataListado.Columns[6].Visible = false;
+            this.dataListado.Columns[8].Visible = false;
+        }
+        //Metodo Mostrar
+        private void Mostrar()
+        {
+            this.dataListado.DataSource = NArticulo.Mostrar();
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros:" + Convert.ToString(dataListado.Rows.Count);
+        }
+        //Metodo BuscarNombre
+        private void BuscarNombre()
+        {
+            this.dataListado.DataSource = NArticulo.BuscarNombre(this.txtBuscar.Text);
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros:" + Convert.ToString(dataListado.Rows.Count);
+        }
+        private void LlenarComboPresentacion()
+        {
+            cbIdpresentacion.DataSource = NPresentacion.Mostrar();
+            cbIdpresentacion.ValueMember = "idpresentacion";
+            cbIdpresentacion.DisplayMember = "nombre";
+        }
+        private void LlenarComboCategoria()
+        {
+            cbIdcategoria.DataSource = NCategoria.Mostrar();
+            cbIdcategoria.ValueMember = "idcategoria";
+            cbIdcategoria.DisplayMember = "nombre";
+        }
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.BuscarNombre();
+        }
+
+        private void dataListado_DoubleClick(object sender, EventArgs e)
+        {
+            frmIngreso form = frmIngreso.GetInstancia();
+            string par1, par2;
+            par1=Convert.ToString(this.dataListado.CurrentRow.Cells["idarticulo"].Value);
+            par2 = Convert.ToString(this.dataListado.CurrentRow.Cells["nombre"].Value);
+            form.setArticulo(par1, par2);
+            this.Hide();
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            this.IsNuevo = true;
+            this.IsEditar = false;
+            this.Botones();
+            this.Limpiar();
+            this.Habilitar(true);
+            this.txtNombre.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rpta = "";
+                if (this.txtNombre.Text == string.Empty)//|| this.txtIdcategoria.Text == string.Empty || this.txtCodigo.Text == string.Empty)
+                {
+                    MensajeError("Falta ingresar algunos datos,serán remarcados");
+                    errorIcono.SetError(txtNombre, "Ingrese un Valor");
+                    errorIcono.SetError(txtCodigo, "Ingrese un Valor");
+                    //  errorIcono.SetError(txtCategoria, "Ingrese un Valor");
+                }
+                else
+                {
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    this.pxImagen.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+
+                    byte[] imagen = ms.GetBuffer();
+
+                    if (this.IsNuevo)
+                    {
+                        rpta = NArticulo.Insertar(this.txtCodigo.Text, this.txtNombre.Text.Trim().ToUpper(),
+                            this.txtDescripcion.Text.Trim(), imagen, Convert.ToInt32(this.cbIdcategoria.SelectedValue),
+                            Convert.ToInt32(this.cbIdpresentacion.SelectedValue));
+                    }
+                    else
+                    {
+                        rpta = NArticulo.Editar(Convert.ToInt32(this.txtIdarticulo.Text),
+                            this.txtCodigo.Text, this.txtNombre.Text.Trim().ToUpper(),
+                            this.txtDescripcion.Text.Trim(), imagen, Convert.ToInt32(this.cbIdcategoria.SelectedValue),
+                            Convert.ToInt32(this.cbIdpresentacion.SelectedValue));
+                    }
+                    if (rpta.Equals("OK"))
+                    {
+                        if (this.IsNuevo)
+                        {
+                            this.MensajeOK("Se Insertó de forma correcta el registro");
+                        }
+                        else
+                        {
+                            this.MensajeOK("Se Actualizó de forma correcta el registro");
+                        }
+                    }
+                    else
+                    {
+                        this.MensajeError(rpta);
+                    }
+                    this.IsNuevo = false;
+                    this.IsEditar = false;
+                    this.Botones();
+                    this.Limpiar();
+                    this.Mostrar();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (!this.txtIdarticulo.Text.Equals(""))
+            {
+                this.IsEditar = true;
+                this.Botones();
+                this.Habilitar(true);
+            }
+            else
+            {
+                this.MensajeError("Debe seleccionar primero el registro a Modificar");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.IsNuevo = false;
+            this.IsEditar = false;
+            this.Botones();
+            this.Limpiar();
+            this.Habilitar(false);
+        }
+    }
+}
---
+++ CapaPresentacion/frmVistaCliente_Venta.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class frmVistaCliente_Venta : Form
+    {
+
+        private bool IsNuevo = false;
+        private bool IsEditar = false;
+
+        public frmVistaCliente_Venta()
+        {
+            InitializeComponent();
+            this.ttMensaje.SetToolTip(this.txtNombre, "Ingrese el nombre o Razon social del cliente");
+            this.ttMensaje.SetToolTip(this.txtDireccion, "Ingrese la direccion del cliente");
+            this.ttMensaje.SetToolTip(this.txtNum_Documento, "Ingrese el numero del documento del cliente");
+        }
+        //Mostrar Mensaje de Confirmacion 
+        private void MensajeOK(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        //Mostrar Mensaje de Error
+
+        private void MensajeError(string mensaje)
+        {
+            MessageBox.Show(mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        //Limpiar todos los controles del formulario
+        private void Limpiar()
+        {
+            this.txtNombre.Text = string.Empty;
+            this.txtNum_Documento.Text = string.Empty;
+            this.txtDireccion.Text = string.Empty;
+            this.txtTelefono.Text = string.Empty;
+            this.txtCorreo.Text = string.Empty;
+            this.txtIdcliente.Text = string.Empty;
+        }
+        //Habilitar los controles del formulario
+        private void Habilitar(bool valor)
+        {
+            this.txtNombre.ReadOnly = !valor;
+            this.cbTipo_Documento.Enabled = valor;
+            this.txtNum_Documento.ReadOnly = !valor;
+            this.txtDireccion.ReadOnly = !valor;
+            this.txtTelefono.ReadOnly = !valor;
+            this.txtCorreo.ReadOnly = !valor;
+            this.txtIdcliente.ReadOnly = !valor;
+        }
+        //Habilitar los Botones
+        private void Botones()
+        {
+            if (this.IsNuevo || this.IsEditar)
+            {
+                this.Habilitar(true);
+                this.btnNuevo.Enabled = false;
+                this.btnGuardar.Enabled = true;
+                this.btnEditar.Enabled = false;
+                this.btnCancelar.Enabled = true;
+            }
+            else
+            {
+                this.Habilitar(false);
+                this.btnNuevo.Enabled = true;
+                this.btnGuardar.Enabled = false;
+                this.btnEditar.Enabled = true;
+                this.btnCancelar.Enabled = false;
+            }
+        }
+        private void frmVistaCliente_Venta_Load(object sender, EventArgs e)
+        {
+            Mostrar();
+            this.Mostrar();
+            this.Habilitar(false);
+            this.Botones();
+        }
+
+        //Metodo para ocultar columnas
+        private void OcultarColumnas()
+        {
+            this.dataListado.Columns[0].Visible = false;
+            this.dataListado.Columns[1].Visible = false;
+        }
+        //Metodo Mostrar
+        private void Mostrar()
+        {
+            this.dataListado.DataSource = NCliente.Mostrar();
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros:" + Convert.ToString(dataListado.Rows.Count);
+        }
+        //Metodo BuscarApellidos
+        private void BuscarNombre()
+        {
+            this.dataListado.DataSource = NCliente.BuscarNombre(this.txtBuscar.Text);
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros:" + Convert.ToString(dataListado.Rows.Count);
+        }
+        //Metodo BuscarNum_Documento
+        private void BuscarNum_Documento()
+        {
+            this.dataListado.DataSource = NCliente.BuscarNum_Documento(this.txtBuscar.Text);
+            this.OcultarColumnas();
+            lblTotal.Text = "Total de Registros:" + Convert.ToString(dataListado.Rows.Count);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (cbBuscar.Text.Equals("Nombre"))
+            {
+                this.BuscarNombre();
+            }
+            else if (cbBuscar.Text.Equals("Documento"))
+            {
+                this.BuscarNum_Documento();
+            }
+        }
+
+        private void dataListado_DoubleClick(object sender, EventArgs e)
+        {
+            frmVenta form = frmVenta.GetInstancia();
+            string par1, par2;
+            par1 = Convert.ToString(this.dataListado.CurrentRow.Cells["idcliente"].Value);
+            par2 = Convert.ToString(this.dataListado.CurrentRow.Cells["nombre"].Value);
+            form.setCliente(par1, par2);
+            this.Hide();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            this.IsNuevo = true;
+            this.IsEditar = false;
+            this.Botones();
+            this.Habilitar(true);
+            this.txtNombre.Focus();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string rpta = "";
+                if (this.txtNombre.Text == string.Empty)
+                {
+                    MensajeError("Falta ingresar algunos datos,serán remarcados");
+                    errorIcono.SetError(txtNombre, "Ingrese un Valor");
+
+                }
+                else
+                {
+                    if (this.IsNuevo)
+                    {
+                        rpta = NCliente.Insertar(this.txtNombre.Text.Trim().ToUpper(),
+                            this.cbTipo_Documento.Text, txtNum_Documento.Text, txtDireccion.Text,
+                            txtTelefono.Text, txtCorreo.Text);
+                    }
+                    else
+                    {
+                        rpta = NCliente.Editar(Convert.ToInt32(this.txtIdcliente.Text),
+                            this.txtNombre.Text.Trim().ToUpper(),
+                            this.cbTipo_Documento.Text, txtNum_Documento.Text, txtDireccion.Text,
+                            txtTelefono.Text, txtCorreo.Text);
+                    }
+                    if (rpta.Equals("OK"))
+                    {
+                        if (this.IsNuevo)
+                        {
+                            this.MensajeOK("Se Inserto de forma correcta el registro");
+                        }
+                        else
+                        {
+                            this.MensajeOK("Se Actualizó de forma correcta el registro");
+                        }
+                    }
+                    else
+                    {
+                        this.MensajeError(rpta);
+                    }
+                    this.IsNuevo = false;
+                    this.IsEditar = false;
+                    this.Botones();
+                    this.Limpiar();
+                    this.Mostrar();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (!this.txtIdcliente.Text.Equals(""))
+            {
+                this.IsEditar = true;
+                this.Botones();
+                this.Habilitar(true);
+            }
+            else
+            {
+                this.MensajeError("Debe seleccionar primero el registro a Modificar");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.IsNuevo = false;
+            this.IsEditar = false;
+            this.Botones();
+            this.Habilitar(false);
+            this.Limpiar();
+            this.txtIdcliente.Text = string.Empty;
+        }
+    }
+}
